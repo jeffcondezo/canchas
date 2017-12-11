48125aebf178010cf7fe32c2c770c17bbb7d84b4
Consultor01 <jpena@creantis.com>
2017-12-11 17:29:58 -0500

    Dia 1

--- maestro/admin.py
+++ maestro/admin.py
@@ -5,6 +5,15 @@ from django.contrib import admin
 
 
 # Register your models here.
-from .models import Deporte
+from .models import Deporte, Subdeporte, Tipocancha, Empresa, Cancha, Sucursal\
+    , Departamento, Provincia, Distrito
 
 admin.site.register(Deporte)
+admin.site.register(Subdeporte)
+admin.site.register(Tipocancha)
+admin.site.register(Empresa)
+admin.site.register(Cancha)
+admin.site.register(Sucursal)
+admin.site.register(Departamento)
+admin.site.register(Provincia)
+admin.site.register(Distrito)
--- maestro/models.py
+++ maestro/models.py
@@ -4,17 +4,71 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class Departamento(models.Model):
+    descripcion = models.CharField(max_length=250)
+
+
+class Provincia(models.Model):
+    descripcion = models.CharField(max_length=250)
+    departamento = models.ForeignKey(Departamento, on_delete=models.PROTECT)
+
+
+class Distrito(models.Model):
+    descripcion = models.CharField(max_length=250)
+    provincia = models.ForeignKey(Provincia, on_delete=models.PROTECT)
+
+
 # Create your models here.
 def deporte_path(instance, filename):
     path = "static/imagenes/deporte/"
     temp = filename.split('.')
-    formato = str(instance.id) + '.' + temp[1]
+    formato = str(instance.descripcion) + '.' + temp[1]
     return path+formato
 
 
 class Deporte(models.Model):
     descripcion = models.CharField(max_length=250)
     imagen = models.ImageField(upload_to=deporte_path)
+
+
+class Subdeporte(models.Model):
+    descripcion = models.CharField(max_length=250)
+    deporte = models.ForeignKey(Deporte, on_delete=models.CASCADE)
+
+
+class Tipocancha(models.Model):
+    descripcion = models.CharField(max_length=250)
+
+
+def empresa_path(instance, filename):
+    path = "static/imagenes/empresa/"
+    temp = filename.split('.')
+    formato = str(instance.ruc) + '.' + temp[1]
+    return path+formato
+
+
+class Empresa(models.Model):
+    descripcion = models.CharField(max_length=250)
+    ruc = models.CharField(max_length=11)
+    imagen = models.ImageField(upload_to=empresa_path)
+
+
+class Sucursal(models.Model):
+    descripcion = models.CharField(max_length=250)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+    distrito = models.ForeignKey(Distrito, on_delete=models.PROTECT)
+    direccion = models.CharField(max_length=250)
+    latitud = models.CharField(max_length=15)
+    longitud = models.CharField(max_length=15)
+
+
+class Cancha(models.Model):
+    descripcion = models.CharField(max_length=250)
+    sucursal = models.ForeignKey(Sucursal, on_delete=models.CASCADE)
+    tipocancha = models.ForeignKey(Tipocancha, on_delete=models.PROTECT)
+
+
+
 
 
 
